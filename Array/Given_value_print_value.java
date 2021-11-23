a9ba7bef5daac347a9db726180e9e512c691f772
Sonu Kumar <81131231+Sonu31@users.noreply.github.com>
2021-11-23 21:01:59 +0530

    Create Given_value_print_value.java

---
+++ Array/Given_value_print_value.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.demo;
+import java.util.Scanner;
+
+public class array {
+    public static void main(String args[]){
+        
+        int arr[] ={11,22,33,44,55};
+        for(int i=0;i<=arr.length;i++){
+            System.out.println("arr is "+arr[i]);
+        }
+        
+    }
+    
+
+}
